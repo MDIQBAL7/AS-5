062f464511acb16ff4aa359528f3c2644ea6dba3
MDIQBAL7 <iqbalhossainbn1@gmail.com>
2025-09-19 01:09:15 +0600

    alert-added

--- app.js
+++ app.js
@@ -12,3 +12,35 @@ for(let i = 0; i < getIcons.length; i++){
     })
 }
 // hard icon functionality end
+
+// call functionality start 
+let getCallIcon = document.getElementsByClassName('call-icon');
+for(let i = 0; i < getCallIcon.length; i++){
+    getCallIcon[i].addEventListener('click', function(){
+        let parents = getCallIcon[i].parentNode.parentNode;
+        let getChild1 = parents.children[1];
+        let getChild2 = getChild1.children[1].innerText;
+        let getChild3 = getChild1.children[2].innerText;
+        // get coin tag 
+        let coin =  document.getElementById('coin');
+        let coinToNum = parseInt(coin.innerText);
+        alert(`${getChild2} ${getChild3}`);  
+        
+       
+          
+        // create an element 
+        let historyContainer = document.getElementById('history-container'); 
+        let div = document.createElement('div');
+        div.innerHTML = `
+            <div class="history-items flex items-center justify-between">
+                <div>
+                   <h1 class="font-bold">${callInfo.title}</h1>
+                <p class="text-gray-500">${callInfo.number}</p>
+                </div>
+                <p class="text-sm">${callInfo.date}</p>
+            </div>
+        `;
+        historyContainer.appendChild(div);
+    })
+} 
+// call functionality end
